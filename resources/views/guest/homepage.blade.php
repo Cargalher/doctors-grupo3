dc365d18d623b51b85f6892935ce8f1eb24601b0
domingo79 <andria140779@yahoo.it>
2021-08-27 19:04:57 +0200

    show controller

--- resources/views/guest/homepage.blade.php
+++ resources/views/guest/homepage.blade.php
@@ -7,12 +7,17 @@
 
     <div class="row">
         @foreach ($doctors as $doc)
-            <a href="{{ route('show', $doc->id) }}">
-                <div class="card col-4">
-                    <h4>nome: {{ $doc->name }}</h4>
-                    <h4>cognome: {{ $doc->lastname }}</h4>
-                </div>
-            </a>
+
+
+            <div class="card col-4">
+                <h4>nome: {{ $doc->name }}</h4>
+                <h4>cognome: {{ $doc->lastname }}</h4>
+                <a href="{{ route('show', $doc->id) }}" class="btn btn-primary">
+                    <i class="fa fa-eye fa-sm fa-fw" aria-hidden="true"></i>
+                </a>
+            </div>
+
+
         @endforeach
     </div>
 @endsection
