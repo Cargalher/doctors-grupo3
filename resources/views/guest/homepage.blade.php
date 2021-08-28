9b6387b61c8ed385f0df7ba0c19ba27279a27666
emaferr <ema.ferr@icloud.com>
2021-08-29 00:15:50 +0200

    Fix layout

--- resources/views/guest/homepage.blade.php
+++ resources/views/guest/homepage.blade.php
@@ -5,19 +5,20 @@
 
 @section('content')
 
-    <div class="row">
-        @foreach ($doctors as $doc)
+<div class="d-flex flex-wrap justify-content-center">
+    @foreach ($doctors as $doc)
 
 
-            <div class="card col-4">
-                <h4>nome: {{ $doc->name }}</h4>
-                <h4>cognome: {{ $doc->lastname }}</h4>
-                <a href="{{ route('show', $doc->id) }}" class="btn btn-primary">
-                    <i class="fa fa-eye fa-sm fa-fw" aria-hidden="true"></i>
-                </a>
-            </div>
+        <div style="width: 300px" class="card m-3 p-3">
+            <h4>nome: {{ $doc->name }}</h4>
+            <h4>cognome: {{ $doc->lastname }}</h4>
+            <a href="{{ route('show', $doc->id) }}" class="btn btn-primary">
+                <i class="fa fa-eye fa-sm fa-fw" aria-hidden="true"></i>
+            </a>
+        </div>
 
 
-        @endforeach
-    </div>
+    @endforeach
+</div>
+
 @endsection
