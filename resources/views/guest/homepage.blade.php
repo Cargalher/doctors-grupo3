3b0c92caf4f573aa2cebf5cdc306f0eb7e20039e
emaferr <ema.ferr@icloud.com>
2021-08-29 00:19:59 +0200

    Add test img

--- resources/views/guest/homepage.blade.php
+++ resources/views/guest/homepage.blade.php
@@ -9,7 +9,8 @@
     @foreach ($doctors as $doc)
 
 
-        <div style="width: 300px" class="card m-3 p-3">
+        <div style="width: 300px" class="card m-3 p-3 ">
+            <img src="{{asset('img/Domenico.png')}}" class="p-2" alt="">
             <h4>nome: {{ $doc->name }}</h4>
             <h4>cognome: {{ $doc->lastname }}</h4>
             <a href="{{ route('show', $doc->id) }}" class="btn btn-primary">
