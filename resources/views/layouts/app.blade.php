2f5d34fc739ee19f8d08ed1547bef0a91592f4ca
Cargalher <cargalher@gmail.com>
2021-09-06 23:34:14 +0200

    Modal to delete doctor account

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                                 Sponsor</a>
                         </li>
 
-                        <li class="nav-item nav-pills">
+                        <!-- <li class="nav-item nav-pills">
                             <form action="{{ route('doctor.destroy', Auth::user()->id) }}" class="nav-link"
                                 method="post">
                                 @csrf
@@ -46,8 +46,39 @@
                                     <i class="fas fa-trash fa-xs fa-fw"></i>
                                 </button>
                             </form>
-                        </li>
+                        </li> -->
+                        
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter">
+                                <i class="fas fa-trash fa-xs fa-fw"></i>
+                                Delete Account
+                            </button>
 
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle"><i class="fas fa-exclamation-triangle text-warning"></i> Are you sure you want to Delete your Account?</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Do you really want to delete these records? This process cannot be undone.
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                            <form action="{{ route('doctor.destroy', Auth::user()->id) }}" class="nav-link"
+                                                method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                     </ul>
                 </aside>
             </div>
